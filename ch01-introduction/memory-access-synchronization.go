f43267f63c99f47cfe9487d0e617a3fe7f8808e8
Mintesnot Shigutie <minte@Mintesnots-Mac-mini.local>
2025-12-23 12:39:10 +0900

    note: memory synchronization pitfalls

--- ch01-introduction/memory-access-synchronization.go
+++ ch01-introduction/memory-access-synchronization.go
@@ -9,15 +9,15 @@ func memoryAccessSynchronization() {
 	var memoryAccess sync.Mutex // 1
 	var data int
 	go func() {
-		memoryAccess.Lock() // 2
-		data++
+		memoryAccess.Lock()   // 2
+		data++                // atomic
 		memoryAccess.Unlock() // 3
 	}()
 	memoryAccess.Lock() // 4
 	if data == 0 {
-		fmt.Printf("the value is %v.\n", data)
+		fmt.Printf("the value is %v.\n", data) // atomic
 	} else {
-		fmt.Printf("the value is %v.\n", data)
+		fmt.Printf("the value is %v.\n", data) // atomic
 	}
 	memoryAccess.Unlock() // 5
 }
@@ -33,3 +33,11 @@ func memoryAccessSynchronization() {
 // 4. Here we once again declare that the following conditional statements should have
 // exclusive access to the data variable’s memory.
 // 5. Here we declare we’re once again done with this memory.
+
+// This example might have solved the data race,but we haven't solved our race condition.
+// The order of operations in this program is still nondeterministic;
+// we’ve just narrowed the scope of the nondeterminism a bit. In this example,
+// either the goroutine will execute first, or both our if and else blocks will.
+// We still don't know which will occur first in any given execution of this program.
+
+// synchronizing access to the memory might cause problems like deadlock, starvation, and live-lock.
